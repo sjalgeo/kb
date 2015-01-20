eb5a5c706797f9e310c1915ae8fcc322b3c71279
Stephen Algeo <stephen.algeo@gmail.com>
2015-01-20 22:23:05 +0000

    Cart updates

--- wp-content/plugins/kavos-tickets/shortcodes/cart.php
+++ wp-content/plugins/kavos-tickets/shortcodes/cart.php
@@ -11,6 +11,30 @@ function sja_kavos_cart_shortcode($atts){
     <div id="cart_holder">
         <?php echo kbGetCartTable(); ?>
     </div>
+
+    <script type="text/javascript">
+        function kbEmptyCart(){
+
+            jQuery.ajax({
+                url: 'http://localhost:8888/kb15/wp-content/plugins/kavos-tickets/ajax/emptycart.php',
+                type: 'POST',
+                data: {
+                    //                eventid: $eventid
+                },
+                success: function(data, textStatus, xhr) {
+
+                    $jsondata = JSON.parse(data);
+                    console.dir($jsondata);
+                },
+                error: function(xhr, textStatus, errorThrown) {
+                    console.log(textStatus.reponseText);
+                }
+            });
+
+        }
+    </script>
+
+
 <?php
 
 }
